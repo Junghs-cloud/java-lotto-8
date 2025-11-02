a8079b43cb145c16843ea5ae5dc8fc13c82337ad
Junghs-cloud <rabbit4935@gmail.com>
2025-11-02 17:30:47 +0900

    Revert "Add: 당첨 번호 로또 입력"
    This reverts commit 20f5fcb9cf6553d93f8db5cad604750b747e98b8.

--- src/main/java/lotto/Controller/LottoController.java
+++ src/main/java/lotto/Controller/LottoController.java
@@ -6,46 +6,24 @@ import lotto.View.*;
 import lotto.Lotto;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class LottoController {
     private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
     private final List<Lotto> releasedLottos = new ArrayList<Lotto>();
 
-    private final static String LOTTO_SPLIT_DELIMITER = ",";
 
     public void execute() {
         int purchaseAmount = getPurchaseInput();
         int lottoAmount = purchaseAmount / Lotto.COST;
-        Lotto firstPrizeLotto = getFirstPrizeLotto();
     }
 
     private int getPurchaseInput() {
         while (true) {
             try {
                 String purchaseAmount = inputView.getPurchaseAmount();
-                //검증
                 return Integer.parseInt(purchaseAmount);
-            } catch (IllegalArgumentException exception) {
-                System.out.println("[ERROR] 입력 값은 정수여야 합니다.");
-            }
-        }
-    }
-
-    private Lotto getFirstPrizeLotto() {
-        while (true) {
-            try {
-                String inputWinningLotto = inputView.getFirstPrizeNumbers();
-                String[] rawLottoNumbers = inputWinningLotto.split(LOTTO_SPLIT_DELIMITER);
-                //검증
-                IntStream rawLottoNumbersStream = Arrays.stream(rawLottoNumbers).mapToInt(Integer::parseInt);
-                List<Integer> lottoNumbers = rawLottoNumbersStream.boxed().toList();
-                return new Lotto(lottoNumbers);
             } catch (IllegalArgumentException exception) {
                 System.out.println("[ERROR] 입력 값은 정수여야 합니다.");
             }
