15786a50dec66f3cdce89370a56f566beafe2329
Junghs-cloud <rabbit4935@gmail.com>
2025-11-02 17:30:39 +0900

    Revert "Add: 입력 수량만큼 로또 발행"
    This reverts commit 0cd6832c5562075c6a3040d036eddaabe6096333.

--- src/main/java/lotto/Model/RandomLottoMaker.java
+++ src/main/java/lotto/Model/RandomLottoMaker.java
@@ -8,15 +8,16 @@ import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomLottoMaker {
+    private final List<Lotto> lottos = new ArrayList<>();
 
-    public static List<Lotto> makeLottos(List<Lotto> lottos, int count) {
+    List<Lotto> makeLottos(int count) {
         for (int i=1;i<=count;i++) {
             lottos.add(makeLotto());
         }
         return lottos;
     }
 
-    private static Lotto makeLotto() {
+    private Lotto makeLotto() {
         List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         Collections.sort(lottoNumbers);
         return new Lotto(lottoNumbers);
