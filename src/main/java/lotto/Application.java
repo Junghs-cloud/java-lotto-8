3c256d44616411be0c74b22cd0de40230fa23bf6
Junghs-cloud <rabbit4935@gmail.com>
2025-11-02 17:56:19 +0900

    일부 삭제 후 재테스트

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,9 @@
 package lotto;
 
-import lotto.Controller.LottoController;
 
 public class Application {
     public static void main(String[] args) {
-        LottoController lottoController = new LottoController();
-        lottoController.execute();
+        //LottoController lottoController = new LottoController();
+        //lottoController.execute();
     }
 }
