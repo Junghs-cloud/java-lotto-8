e534fd18dde6fe71cf3aab465da0b29155440e7f
Junghs-cloud <rabbit4935@gmail.com>
2025-11-02 17:13:25 +0900

    Modify: 테스트 수정

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -13,7 +13,7 @@ class ApplicationTest extends NsTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
 
     @Test
-    void 기능_테스트() {
+    void testAll() {
         assertRandomUniqueNumbersInRangeTest(
                 () -> {
                     run("8000", "1,2,3,4,5,6", "7");
@@ -80,7 +80,7 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void testPurchaseAmountNotInt() {
         assertSimpleTest(() -> {
             runException("1000j");
             assertThat(output()).contains(ERROR_MESSAGE);
@@ -104,7 +104,7 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void testWinningLottoNotSize6() {
+    void testWinningLottoSizeNotCorrect() {
         assertSimpleTest(() -> {
             runException("3000", "1,2,3,4,5,6,7");
             assertThat(output()).contains(ERROR_MESSAGE);
